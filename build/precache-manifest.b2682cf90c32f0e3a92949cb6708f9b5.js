d7dd466b2a8fc75f36c9a9d395861259c2a0da42
Md afratul kaoser Taohid <taohid32@gmail.com>
2020-12-10 16:36:46 +0600

    added production build

---
+++ build/precache-manifest.b2682cf90c32f0e3a92949cb6708f9b5.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc24859ab98b0039ca4949211e109aa2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80e24f4ee87b0f1db174",
+    "url": "/static/css/2.53151e9a.chunk.css"
+  },
+  {
+    "revision": "3ec3d4ccabeef90d29b7",
+    "url": "/static/css/main.560f2943.chunk.css"
+  },
+  {
+    "revision": "80e24f4ee87b0f1db174",
+    "url": "/static/js/2.5ecabd04.chunk.js"
+  },
+  {
+    "revision": "a2c280725a84207aa0b2f2c33eba33ab",
+    "url": "/static/js/2.5ecabd04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ec3d4ccabeef90d29b7",
+    "url": "/static/js/main.01b6bde2.chunk.js"
+  },
+  {
+    "revision": "cb157c16e8952228dd98",
+    "url": "/static/js/runtime-main.8c1fa264.js"
+  },
+  {
+    "revision": "ceb57853dd178cfe932b84f1cb514ee2",
+    "url": "/static/media/1.ceb57853.jpg"
+  },
+  {
+    "revision": "967f0d216900aa13725d0956baca40fb",
+    "url": "/static/media/2.967f0d21.jpg"
+  },
+  {
+    "revision": "2820d1041b4c8f5c276e9f49e12bacde",
+    "url": "/static/media/3.2820d104.jpg"
+  },
+  {
+    "revision": "cebaf54eee9e90f6cb8bea9706f604df",
+    "url": "/static/media/4.cebaf54e.jpg"
+  },
+  {
+    "revision": "8e5ecd61834cd3423e58ecdcff65219c",
+    "url": "/static/media/ColorWebLogo.8e5ecd61.png"
+  },
+  {
+    "revision": "b0b9bec890faedc4d41596ddb4e295b9",
+    "url": "/static/media/avatar.b0b9bec8.png"
+  },
+  {
+    "revision": "984a955a22925312def22ececfa01df4",
+    "url": "/static/media/banner.984a955a.jpg"
+  },
+  {
+    "revision": "c23a77ce94710b577e79238f8e876a06",
+    "url": "/static/media/bars-two.c23a77ce.jpg"
+  },
+  {
+    "revision": "6a3bdd18f401d12720794d208c13202d",
+    "url": "/static/media/bg-video-1.6a3bdd18.mp4"
+  },
+  {
+    "revision": "3963c6ba8219b908473eb757132d6178",
+    "url": "/static/media/cafe.3963c6ba.jpg"
+  },
+  {
+    "revision": "5acaa8ec432088463512bf363954be87",
+    "url": "/static/media/popular-five.5acaa8ec.jpg"
+  },
+  {
+    "revision": "f8ce437fdffac5e40f645320f3b35af7",
+    "url": "/static/media/popular-four.f8ce437f.jpg"
+  },
+  {
+    "revision": "0f67463670de09c08ce7db6684530afd",
+    "url": "/static/media/popular-one.0f674636.jpg"
+  },
+  {
+    "revision": "b4c42ae087c185a21cd0fabc079ba373",
+    "url": "/static/media/popular-six.b4c42ae0.jpg"
+  },
+  {
+    "revision": "0fe559d79fbf25edcda7b51b67887057",
+    "url": "/static/media/popular-three.0fe559d7.jpg"
+  },
+  {
+    "revision": "448b5875e8426e26de4d0b5db9632c6c",
+    "url": "/static/media/pressumBackground.448b5875.svg"
+  },
+  {
+    "revision": "aa720613c8b646c8d4498a21e606468d",
+    "url": "/static/media/private-events-banner.aa720613.jpg"
+  },
+  {
+    "revision": "4ffa147db702720c23664462dbb919e0",
+    "url": "/static/media/private-events-best.4ffa147d.jpg"
+  },
+  {
+    "revision": "41127873ed5903d1db23dcb89caecbb3",
+    "url": "/static/media/private-events-review.41127873.jpg"
+  },
+  {
+    "revision": "6c92750cbcf9d2880e6b58c0f275d578",
+    "url": "/static/media/profile.6c92750c.jpg"
+  },
+  {
+    "revision": "eafbb513fe6b6469fe226a0aea1bd2e0",
+    "url": "/static/media/restaurant.eafbb513.jpg"
+  },
+  {
+    "revision": "68fc9da8012081f4df0f10821325c92e",
+    "url": "/static/media/thali.68fc9da8.jpg"
+  },
+  {
+    "revision": "86591ad7f052d582c27a70a1504a5a4c",
+    "url": "/static/media/upcomming-four.86591ad7.jpg"
+  },
+  {
+    "revision": "daa407649ded9c5037852c78e36a8f67",
+    "url": "/static/media/upcomming-two.daa40764.jpg"
+  }
+]);
